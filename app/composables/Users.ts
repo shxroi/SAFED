ead9a12a9a466e48974e214cd75f8692db550905
Norbertino Rakha <rakhaboy2590@gmail.com>
2026-01-22 11:32:52 +0800

    make composable for users management

---
+++ app/composables/Users.ts
@@ -0,0 +1,39 @@
+import type { User } from '../../shared/types/user';
+
+export const useUsers = () => {
+  const getUsers = async () => {
+    return await $fetch<{ users: User[] }>('/api/users');
+  }
+
+  const getUser = async (id: number) => {
+    return await $fetch<{ user: User }>(`/api/users/${id}`);
+  }
+
+  const createUser = async (userData: Omit<User, 'id' | 'status'>) => {
+    return await $fetch<{ success: boolean; user: User }>('/api/users', {
+      method: 'POST',
+      body: userData
+    })
+  }
+
+  const updateUser = async (id: number, userData: Partial<User>) => {
+    return await $fetch<{ success: boolean; user: User }>(`/api/users/${id}`, {
+      method: 'PUT',
+      body: userData
+    })
+  }
+
+  const deleteUser = async (id: number) => {
+    return await $fetch<{ success: boolean, message: string }>(`/api/users/${id}`, {
+      method: 'DELETE'
+    })  
+  }
+
+  return {
+    getUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+  }
+}
